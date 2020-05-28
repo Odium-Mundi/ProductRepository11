28ce983abaaded3fdb3d685846ab75ce3a363749
Odium-Mundi <violetta_wolf@mail.ru>
2020-05-28 06:56:15 +0300

    class advance

--- src/main/java/domain/Book.java
+++ src/main/java/domain/Book.java
@@ -1,4 +1,19 @@
 package domain;
 
-public class Book {
+public class Book extends Product{
+    private String author;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Book(int id, String name, int price, String author) {
+        super(id, name, price);
+        this.author = author;
+
+    }
 }
--- src/main/java/domain/Smartphone.java
+++ src/main/java/domain/Smartphone.java
@@ -1,4 +1,17 @@
 package domain;
 
-public class Smartphone {
-}
+public class Smartphone extends Product{
+    private String producer;
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public Smartphone(int id, String name, int price, String producer) {
+        super(id, name, price);
+        this.producer = producer;
+    }       }
